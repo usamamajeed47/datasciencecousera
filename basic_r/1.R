520610f30bc82077d9068e92a9b5461122390877
Muhammad usama majeed <usamamajeed@gcuf.edu.pk>
2025-06-30 18:41:44 +0500

    basics_of_r

---
+++ basic_r/1.R
@@ -0,0 +1,14 @@
+print("hello world")
+avg <- function(){ 
+x = rnorm(100)
+mean(x)
+}
+
+second <- function(x) {
+ x + rnorm(length(x)) 
+} 
+
+# assign values to variables 
+
+x <-1  
+msg <- "hello"
